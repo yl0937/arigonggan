5f82e4402602464bcc7742ae0897b12470c34653
yl0937 <60501504+yl0937@users.noreply.github.com>
2022-05-25 04:04:35 +0900

    SignUp

--- arigonggan/models.py
+++ arigonggan/models.py
@@ -1,6 +1,10 @@
-from django.db import models
+import pymysql
 
-# Create your models here.
-class Seat(models.Model):
-    name = models.CharField(max_length=20)
-    status = models.CharField(max_length=15)
+conn = pymysql.connect(host='localhost', user='root', password='lim1231', db='goorm', charset='utf8')
+
+def usernser(infoQuery):
+    cur = conn.cursor()
+    sql = "insert into user (userId, password) VALUES (%s,%s)"
+    cur.execute(sql,infoQuery)
+    conn.commit()
+    conn.close()
--- arigonggan/views.py
+++ arigonggan/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse,HttpResponse
 import pymysql
+import json
 from django.views.decorators.csrf import csrf_exempt
-
+from django.utils.decorators import method_decorator
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os, time, traceback
+from arigonggan import models
+import bcrypt
+from config.settings import SECRET_KEY
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 Pybo에 오신것을 환영합니다.")
+    return HttpResponse("replace main")
 
-@csrf_exempt
+@method_decorator(csrf_exempt,name='dispatch')
 def signup(requset):
-    return HttpResponse("성공")
+    data = json.loads(requset.body)
+    userId = data['userId']
+    pwd = data['password'].encode('utf-8')
+    password_crypt = bcrypt.hashpw(pwd, bcrypt.gensalt())
+    password_crypt = password_crypt.decode('utf-8')
+    query=(userId,password_crypt)
+    try:
+        models.usernser(query)
+        return JsonResponse({'message':'SUCCESS'},status=200)
+    except:
+        return JsonResponse({'message':'DBERR'},status=400)
