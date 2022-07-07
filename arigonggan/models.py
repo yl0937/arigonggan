bf79a5bba35b93e730cf55fde72c289dee7d739b
sunwooan <tjsdn3410897@gmail.com>
2022-07-07 15:08:52 +0900

    merge

--- arigonggan/models.py
+++ arigonggan/models.py
@@ -1,9 +1,5 @@
 import pymysql
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d7620c657a6246d7e56503a812eb90dc0a0ddee8
 
 from django.db import models
 
@@ -17,10 +13,7 @@ class Question(models.Model):
 
 def usernser(infoQuery):
     conn = pymysql.connect(host='localhost', user='master', password='master', db='goorm', charset='utf8')
-<<<<<<< HEAD
 
-=======
->>>>>>> d7620c657a6246d7e56503a812eb90dc0a0ddee8
     cur = conn.cursor()
     sql = "insert into User (userId) VALUES (%s)"
     res = cur.execute(sql,infoQuery)
@@ -31,10 +24,7 @@ def usernser(infoQuery):
 
 def login(infoQuery):
     conn = pymysql.connect(host='localhost', user='master', password='master', db='goorm', charset='utf8')
-<<<<<<< HEAD
 
-=======
->>>>>>> d7620c657a6246d7e56503a812eb90dc0a0ddee8
     cur = conn.cursor()
     sql = "select * from User where userId = %s"
     res = cur.execute(sql,infoQuery)
@@ -45,10 +35,7 @@ def login(infoQuery):
 
 def delete(infoQuery):
     conn = pymysql.connect(host='localhost', user='master', password='master', db='goorm', charset='utf8')
-<<<<<<< HEAD
 
-=======
->>>>>>> d7620c657a6246d7e56503a812eb90dc0a0ddee8
     cur = conn.cursor()
     sql = "update Reservation set status = 'deactivate' where id = %s and status = 'activate'"
     sta = cur.execute(sql, infoQuery)
